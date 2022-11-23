40fb4e75a9b21a547319f4dfb008168ced531361
Felix <felixnzomo@gmail.com>
2022-11-23 17:03:21 +0300

    Add dynamic funtion for the speakers section

--- script.js
+++ script.js
@@ -3,82 +3,63 @@
 const speakersData = [
   {
     photo: './images/Speakers-01.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
   {
     photo: './images/Speakers-02.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
   {
     photo: './images/Speakers-03.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
   {
     photo: './images/Speakers-04.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
   {
     photo: './images/Speakers-05.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
   {
     photo: './images/Speaker-06.png',
+    name: 'Hellen john',
     title: 'CE0 of Africa Tech club',
     description:
       'Founder of Brits-Tech, with 15 years experience in technology and innovation',
   },
 ];
 
-const dynamic = document.querySelector('.speaker-area');
-dynamic.innerHTML = `<div class='speakers-detail'>
-          
-          <img src="${speakersData[0].photo}" class="image" 
-        </div>
+const speakerDetails = document.querySelector('.speaker-area');
+
+speakersData.forEach((speaker) => {
+  speakerDetails.innerHTML += `<div class='speakers-detail'>
+          <img src="${speaker.photo}" class="image" ></img>
         <div class='col-2'>
-          <h3>Lorna Gig</h3>
-          <p style="color:#ed5342"><em>${speakersData[0].title}</em></p>
+          <h3>${speaker.name}</h3>
+          <p style="color:#ed5342"><em>${speaker.title}</em></p>
           <span></span>
           <p>
-          ${speakersData[0].description} 
+          ${speaker.description} 
           </p>
         </div>
-        </div>
-
-
-        <div class='speakers-detail'>
-          
-          <img src="${speakersData[0].photo}" class="image" 
-        </div>
-        <div class='col-2'>
-          <h3>Lorna Gig</h3>
-          <p style="color:#ed5342"><em>${speakersData[0].title}</em></p>
-          <span></span>
-          <p>
-          ${speakersData[0].description} 
-          </p>
-        </div>
-        </div>
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        `;
+        </div> `;
+});
 
 // --------------------- Mobile-Menu------------------
 
@@ -90,5 +71,5 @@ hamburger.onclick = function showMenu() {
   mobileMenu.style.display = 'block';
 };
 closeBtn.onclick = function closeMenu() {
-  mobileMenu.style.display = 'none';
+  mobileMenu.style.display = none = 'none';
 };
